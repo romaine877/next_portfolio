314de78ecde79a14aca6b180ab9162930ee66252
romaine877 <romaine877@gmail.com>
2022-01-23 04:08:14 -0500

    config file

---
+++ config/index.js
@@ -0,0 +1,5 @@
+export const projectsApi = process.env.API_URL
+export const emailApi = process.env.EMAIL_API 
+export const sendGridApiKey = process.env.SENDGRID_KEY
+export const recieving_email = process.env.RECIPIENT_EMAIL 
+export const sending_email = process.env.SENDER_EMAIL 
