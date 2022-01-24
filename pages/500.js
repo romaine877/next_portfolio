bcf11e7476d6251629ffbead8074f7f46db8bcd8
romaine877 <romaine877@gmail.com>
2022-01-23 19:20:16 -0500

    404,500 pages adjusted

---
+++ pages/500.js
@@ -0,0 +1,30 @@
+import Layout from "../components/Layout"
+import Link from "next/link"
+
+
+
+export default function ServerError() {
+  return (
+    <Layout title={'500'}>
+        <div className="md:h-5/6 flex items-center">
+            <div className=" w-5/6 mx-auto ">
+                <h1 className="text-5xl font-bold pb-6 dark:text-white text-black text-center">
+                    500
+                </h1>
+                <p className="dark:text-white text-black text-center">
+                    Internal Server Error, please try again later.
+                </p>
+                <div className="flex justify-center my-5">
+
+                <Link href="/">
+                    <a className="bg-blue-500 hover:bg-indigo-700 hover:scale-105 transition ease-in m-3 mb-20 py-3 px-4 dark:text-white text-black font-bold rounded-md flex">
+                        Go Home
+                    </a>
+                </Link>
+                </div>
+            </div>
+        </div>
+    </Layout>
+
+  )
+}
