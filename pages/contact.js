fda8d3167a6d98bda6badf55639a2cccf15cdb4f
romaine877 <romaine877@gmail.com>
2024-10-11 19:00:40 -0500

    update email

--- pages/contact.js
+++ pages/contact.js
@@ -147,22 +147,9 @@ export default function Contact() {
             </p>
           </div>
           <div className="mb-10">
+         
             <a
-              href="tel:8768968976"
-              className="text-base bg-blue-500 w-72 hover:bg-indigo-700 hover:scale-105 transition ease-in m-3 py-3 px-4 text-white font-bold rounded-md flex justify-start mb-5"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5 mr-3 dark:text-white"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
-              </svg>
-              +876-896-8976
-            </a>
-            <a
-              href="mailto:romaine877@gmail.com"
+              href="mailto:developer@romaine.dev"
               className="text-base bg-blue-500 w-72 hover:bg-indigo-700 hover:scale-105 transition ease-in m-3 py-3 px-4 text-white font-bold rounded-md flex justify-start"
             >
               <svg
@@ -174,7 +161,7 @@ export default function Contact() {
                 <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
                 <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
               </svg>
-              romaine877@gmail.com
+              developer@romaine.dev
             </a>
           </div>
           <div className="flex w-1/3 m-5 justify-between">
