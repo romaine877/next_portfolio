6c9d9af0a055ccab74ee4b06b7367e1a499f3459
romaine877 <romaine877@gmail.com>
2024-10-11 20:10:34 -0500

    projects fix

--- components/ProjectModal.js
+++ components/ProjectModal.js
@@ -1,6 +1,6 @@
 import { motion } from 'framer-motion';
 import React from 'react';
-import Image from 'next/image';
+import Image from "next/legacy/image";
 
 export default function ProjectModal({ id, image, name, description, link, selectHandler }) {
     
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Layout from "../components/Layout";
 import Link from "next/link";
-import Image from "next/image";
+import Image from "next/legacy/image";
 import { motion } from "framer-motion";
 
 export default function Home() {
